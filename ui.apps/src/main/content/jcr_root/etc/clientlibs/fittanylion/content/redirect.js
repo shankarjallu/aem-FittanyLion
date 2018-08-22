e664eeb0b95bc2529f206b0804eba5cb69d0b6c8
gowrishankarjallu <gowrishankar.jallu@gmail.com>
2018-08-22 15:04:46 -0400

    clientlib changes

--- ui.apps/src/main/content/jcr_root/etc/clientlibs/fittanylion/content/redirect.js
+++
@@ -1,7 +0,0 @@
-function urlRedirect(){
-     var currentPath = window.location.pathname;
-            if (currentPath == 'wpa') {    
-                  $("#header a img").attr("src", "/content/dam/digital-marketing/highmark/purl/highmark-bcbs-delaware-logo.png");
-            }
-
-}
