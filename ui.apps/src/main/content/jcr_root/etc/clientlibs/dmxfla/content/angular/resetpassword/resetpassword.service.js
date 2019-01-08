1adefbfdaec8f97757bafc0d859ad1cfeacd63db
gowrishankarjallu <gowrishankar.jallu@gmail.com>
2019-01-08 16:01:41 -0500

    Reset Password changes

--- ui.apps/src/main/content/jcr_root/etc/clientlibs/dmxfla/content/angular/resetpassword/resetpassword.service.js
+++ ui.apps/src/main/content/jcr_root/etc/clientlibs/dmxfla/content/angular/resetpassword/resetpassword.service.js
@@ -1,15 +1,22 @@
 (function() {
     angular.module('fittanyUiApp')
 
-    .factory('ResetPasswordService', ['$http', '$timeout', 'Base64',
-        function($http, $timeout, Base64) {
+    .factory('ResetPasswordService', ['$http', '$timeout', 'Base64', '$location',
+        function($http, $timeout, Base64, $location) {
             var service = {};
 
 
        service.Reset = function(user) {
            var custnewpwd = Base64.encode(user.password);
+
+
+    var outputJson = $location.search();
+       var key = $location.search().key;
+
+
+
            var url = '/bin/getForgotPasswordServlet';
-                 url = url + '?custnewPassword='+ custnewpwd;
+                 url = url + '?custnewPassword='+ custnewpwd + '&key=' + key;
 
 
       return $http.post(url);
